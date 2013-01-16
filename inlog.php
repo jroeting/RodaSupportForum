e2f0213132503074e88ca8994be0a4656197d7dd
Max Bevelander <maxbevelander@hotmail.com>
2013-01-16 22:27:51 +0100

    Login form toegevoegd
    hij checkt nog niet met de databasa online, moet nog even uitzoeken hoe
    dat te doen. Als het goed is encrypt hij ook wachtwoorden, maar ook dit
    weet ik niet zeker

--- inlog.php
+++ inlog.php
@@ -4,17 +4,19 @@
     <form>
 		<table>
 			<tr>
+				<form name="login_form" method="post" action="checklogin.php">
 				<td>Username:</td>
-				<td><input type="text" name="username"></td>
+				<td><input type="text" name="username" id="myusername"></td>
 			</tr>
 			<tr>
 				<td>Password:</td>
-				<td><input type="password" name="password"></td>
+				<td><input type="password" name="password" id="mypassword"></td>
+				</form>
 			</tr>
 		</table>
 		<a href="index.php?content=retrievepassword">password forgotten?</a><br /> 
 		<a href="index.php?content=register"> register for an account</a><br /><br />
         <input type="checkbox" name="remember" value="me"> Remember me <br />
-		<input type="submit" value="Sign in" style="margin:5px";>
+		<input type="submit" value="Sign in" name="submit" style="margin:5px";>
 	</form>
 </div>
---
+++ login_success.php
@@ -0,0 +1,13 @@
+// Check if session is not registered, redirect back to main page. 
+ <?php
+ session_start();
+ if(!session_is_registered(myusername)){
+ header("location:login.php");
+ }
+ ?>
+
+ <html>
+ <body>
+ Login Successful
+ </body>
+ </html>
