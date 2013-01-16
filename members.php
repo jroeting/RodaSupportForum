0670d022f5e4108c7b6dd546cc57ca520b307db0
Michael Chen <mickeywhc@hotmail.com>
2013-01-16 15:18:00 +0100

    members
    members.php file

---
+++ members.php
@@ -0,0 +1,43 @@
+	<h1>
+		Members of Roda
+	</h1>
+
+<?php
+	$link = mysql_connect("localhost:3306","webdb13KIC1","busteqec");
+	if (!$link)
+	{
+		die('Could not connect: ' . mysql_error());
+	}
+	//echo "Connected";
+	$selected_db = mysql_select_db('user_data', $link);
+	if (!$selected_db)
+	{
+		die('Cannot use user_data:' . mysql_error());
+	}
+	
+	$result = mysql_query("SELECT * FROM user_data ORDER BY username LIMIT 0, 10"); 
+	if (!$result) 
+	{
+		echo 'Could not run query: ' . mysql_error();
+		exit;
+	}
+
+	$num=mysql_fetch_row($result); 
+
+	$i=0; 
+	while ($i < $num) { 
+		$username=mysql_result($result,$i,"username"); 
+		$email=mysql_result($result,$i,"email"); 
+		$account_type=mysql_result($result,$i,"account_type"); 
+		
+		echo "<b>$username</b>E-mail: $email1<br>Account Type: $account_type<br><hr><br>"; 
+
+		$i++; 
+	} 
+	mysql_close($link);
+?>	
+	
+	
+	
+		
+
