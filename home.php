7dc56140d49d6554d2a810b5e84625382a1afc02
jroeting <jennifer.roeting@gmail.com>
2013-01-16 12:46:22 +0100

    Update home.php

--- home.php
+++ home.php
@@ -1,3 +1,31 @@
-	<p>
-		Welcome to Roda Support Forum
-	</p>
+
+
+<div id="newestposts">
+	<table class="forumcontent" align="center">
+  		<tr>
+    		<td><strong>10 most recent subjects</strong></td>
+        </tr>
+        </table>
+        <table border="0px" bgcolor="#333333">
+        <?php
+		$con = mysql_connect("localhost:3306","webdb13KIC1","busteqec"); 
+		if(!$con)
+		{
+			die('Could not connect ' . mysql_error());
+		}
+		$selected_db = mysql_select_db("webdb13KIC1",$con);
+		$selection = mysql_query("SELECT * FROM subjects WHERE open=1 ORDER BY subject_id DESC LIMIT 0,10");
+		while($row = mysql_fetch_array($selection))
+  		{
+  			echo "<tr>";
+  			echo "<td>" . $row['subject_name'] . "</td>";
+  			echo "</tr>";
+  		}
+  	?>
+	</table>
+  	<table class="forumcontent" align="center">
+	  	<tr>
+   			<td><strong>10 most popular subjects</strong></td>
+  		</tr>
+	</table>
+</div>
