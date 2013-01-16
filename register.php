25c79cab1b0695b2b12c2dd91e379b2e4b7a9b10
Kyllian Broers <kyllianbroers@live.nl>
2013-01-16 13:09:25 +0100

    bezig met register

--- register.php
+++ register.php
@@ -1,15 +1,31 @@
 	<div id="registrationcontent">
         <p class="registration">&nbsp;</p>
         <p class="registration">Please fill in the form to register to this forum</p>
-        <form >
+        <form action="index.php?content=register" method="post">
+			<?php
+				if (isset($_POST["submit"])) 
+				{
+					checkName();
+					checkSurename();
+					checkEmail();
+					checkUsername();
+					checkPassword();
+				}
+			?>
 			<table class="registration">
                 <tr>
                     <td class="leftcolum">Name</td>
-                    <td class="rightcolum"><input type="text" name="name" /></td>
+                    <td class="rightcolum"><input type="text" name="name" />
+						
+					</td>
                 </tr>
                 <tr>
                     <td class="leftcolum">Surname</td>
                     <td class="rightcolum"><input type="text" name="surname" /></td>
+                </tr>
+				<tr>
+                    <td class="leftcolum">Infix</td>
+                    <td class="rightcolum"><input type="text" name="infix" /></td>
                 </tr>
 				<tr>
                     <td class="leftcolum">E-mail</td>
@@ -18,6 +34,10 @@
                 <tr>
                     <td class="leftcolum">Username</td>
                     <td class="rightcolum"><input type="text" name="username" /></td>
+                </tr>
+				<tr>
+                    <td class="leftcolum">Quote</td>
+                    <td class="rightcolum"><input type="text" name="Quote" /></td>
                 </tr>
                 <tr>
                     <td class="leftcolum">Password</td>
@@ -28,7 +48,7 @@
                     <td class="rightcolum"><input type="password" name="passwordcheck" id="passwordcheck"/></td>
                 </tr>
                 <tr>
-                    <td colspan="2" class="submit"><input type="submit" value="Register"></td>
+                    <td colspan="2" class="submit"><input type="submit" value="register" name="submit"></td>
                 </tr>
             </table>
         </form>
