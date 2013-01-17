8bf5c599cecefde6d57a8d8fe84d0355c41bc8a8
Max Bevelander <maxbevelander@hotmail.com>
2013-01-17 20:55:35 +0100

    aanpassingen

--- checklogin.php
+++ checklogin.php
@@ -1,14 +1,28 @@
 <?php
-// Connect to server and select databse.
- mysql_connect("localhost:3306", "webdb13KIC1", "busteqec")or die("cannot connect"); 
- mysql_select_db("webdb13KIC1")or die("cannot select DB");
-
 // username and password sent from form 
- $myusername=$_POST['myusername']; 
- $mypassword=$_POST['mypassword'];
+$myusername=$_POST['myusername']; 
+$mypassword=$_POST['mypassword'];
 
 // encrypt password
 $mypassword = hash('sha256', $mypassword);
+
+// Connect to server and select databse.
+$connectie = mysql_connect("localhost:3306", "webdb13KIC1", "busteqec");
+if(!$connectie)
+{
+	die('Could not connect to database ' . mysql_error());
+}
+$select_db = mysql_select_db("webdb13KIC1", $connectie);
+if(!select_db)
+{
+	die('Could not connect ' . mysql_error());
+}
+$select = mysql_query("SELECT * FROM $user_data WHERE username='$myusername' and password='$mypassword'");
+if(!$select)
+{
+	echo 'Could not run query: ' . mysql_error();
+	exit;
+}
 
 // To protect MySQL injection
  $myusername = stripslashes($myusername);
@@ -16,15 +30,10 @@ $mypassword = hash('sha256', $mypassword);
  $myusername = mysql_real_escape_string($myusername);
  $mypassword = mysql_real_escape_string($mypassword);
 
-$sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
- $result=mysql_query($sql);
-
-
 // Mysql_num_row is counting table row
  $count=mysql_num_rows($result);
 
 // If result matched $myusername and $mypassword, table row must be 1 row
-
 if($count==1){
 
 // Register $myusername, $mypassword and redirect to file "login_success.php"
