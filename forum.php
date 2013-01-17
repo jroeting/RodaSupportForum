5e8de857592c47adf5bf946fe18d5291386bb13c
Jennifer Roeting <jennifer.roeting@gmail.com>
2013-01-17 15:27:00 +0100

    pagina's toegevoegd

--- forum.php
+++ forum.php
@@ -5,12 +5,12 @@
     <p style="h1">&nbsp; &nbsp; Categories</p></strong></td>
   </tr>
   <tr class="tablebody">
-  	<td><p class="blacktext"><a href="index.php?content=technical_issues" <?php if($content == home) echo "class=\"current\"" ?>><span>Technical Issues</span></a></p></td>
+  	<td><p class="blacktext"><a href="index.php?content=technical_issues" <?php if($content == technical_issues) echo "class=\"current\"" ?>><span>Technical Issues</span></a></p></td>
   </tr>
   <tr class="tablebody">
-  	<td><p class="blacktext">Cartalk</p></td>
+  	<td><p class="blacktext"><a href="index.php?content=cartalk" <?php if($content == cartalk) echo "class=\"current\"" ?>><span>Cartalk</span></a></p></td>
   </tr>
   <tr class="tablebody" >
-  	<td><p class="blacktext">Car-unrelated</p></td>
+  	<td><p class="blacktext"><a href="index.php?content=car_unrelated" <?php if($content == car_unrelated) echo "class=\"current\"" ?>><span>Car-unrelated</span></a></p></td>
   </tr>
 </table>
