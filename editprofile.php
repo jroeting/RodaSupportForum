2c0a8747ea361eb5514aee0c5610ff1f49da2b68
Michael Chen <mickeywhc@hotmail.com>
2013-01-18 23:38:54 +0100

    Voorlopig Layout voor profile en editprofile
    De php erachter komt nog

---
+++ editprofile.php
@@ -0,0 +1,36 @@
+<div>
+        <div class="tablehead"> <strong> Edit Your Profile </strong></div>
+		
+        <form action="index.php?content=editprofile" method="post">
+			<table cellpadding="10">
+				<tr>
+					<td> Avatar: </td> <td> <input type="file" name="avatar" size="100" value="Browse"></td>
+				</tr>
+				<tr>
+					<td>Personal Text: </td> <td><textarea cols="50" rows="5" name="perstxt">Type here your personal text for your profile visiters.</textarea></td>	
+				</tr>
+				<tr>
+					<td> Age: </td> 
+					<td><input type="text" name="age"></td>	
+				</tr>
+				<tr>
+					<td>Gender: </td> 
+					<td><select name="cars">
+						<option value="male">Male</option>
+						<option value="female">Female</option>
+						</select>
+					</td>	
+				</tr>
+				<tr>
+					<td> Country </td> 
+					<td><input type="text" name="country"></td>	
+				</tr>
+				<tr><td>Quote: </td> <td><textarea cols="50" rows="5" name="quote">Type here your signature/quote.</textarea></td></td></tr>
+				<tr>
+				</tr>
+				<tr>
+					<td><input type="submit" value="Change Profile"></td>
+				</tr>
+			</table>
+		</form>
+</div>
---
+++ profile.php
@@ -0,0 +1,25 @@
+<div class="tablehead"> <strong> Profile </strong></div>
+
+
+<div class="profilecontent">
+	<div class="userprofile1">
+		<div class="avatar"> Avatar.jpg </div>
+		<br />
+		Username: <br /><br />
+		Account Type: <br /><br />
+		<a href="#"> Send PM </a> <br /><br />
+	</div>
+		
+	<div class="userprofile2">
+		Personal Text: <br /><br />
+		First Name: <br /><br />
+		Last Name: <br /><br />
+		Age: <br /><br />
+		Gender: <br /><br />
+		Country: <br /><br />
+		Quote: <br /><br />
+		<a href="index.php?content=editprofile"> <i> <strong> Edit Profile </strong> </i> </a> <br /> <br />
+	</div>
+	
+</div> 
+
