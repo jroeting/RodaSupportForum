cd1cb929b3cfc34785e0810872b0bc34d0cca87c
jroeting <jennifer.roeting@gmail.com>
2013-01-18 10:47:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -51,12 +51,12 @@
 			}
 			?>
         </div>
-        <div id="footer">Copyright &#169; 2013 Disclaimer <a href="mailto:123@abc.nl">Contact</a>
+        <div id="footer">
         	<table>
         		<tr>
                 	<td width="190">Copyright &#169; 2013</td>
                     	<td width="190">Disclaimer</td>
-                    	<td width="350"><a href="mailto:123@abc.nl">Contact</a></td>
+                    	<td width="400"><a href="mailto:123@abc.nl">Contact</a></td>
                     	<td width="170"><a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="15px" width="auto" /></a>
 			<a href="http://jigsaw.w3.org/css-validator/check/referer"><img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" height="15px" width="auto"/></a></td>
 		</table>
