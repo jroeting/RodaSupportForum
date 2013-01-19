48464a18029cefcff29e3c7bf48d5a07859a59fc
Kyllian Broers <kyllianbroers@live.nl>
2013-01-19 18:21:26 +0100

    comment in php gezet

--- login_success.php
+++ login_success.php
@@ -1,5 +1,5 @@
-// Check if session is not registered, redirect back to main page. 
- <?php
+<?php
+ // Check if session is not registered, redirect back to main page. 
  session_start();
  if(session_is_registered($myusername))
  {
