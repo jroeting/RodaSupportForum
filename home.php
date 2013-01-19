86a3c348bc740b4281c83a98da5ecb546376952b
Jennifer Roeting <jennifer.roeting@gmail.com>
2013-01-19 10:16:23 +0100

    update

--- home.php
+++ home.php
@@ -1,34 +1,29 @@
 <div id="newestposts">
-	<table class="forumcontent" align="center">
+	<table class="tablemember" celpadding="10">
   		<tr>
-    		<td class="tablehead"><strong>10 most recent subjects</strong></td>
+    		<th class="tablehead"><strong>10 most recent subjects</strong></th>
         </tr>
-        </table>
-        <table>
-        	<?php
-				$con = mysql_connect("localhost:3306","webdb13KIC1","busteqec"); 
-				if(!$con)
-				{
-					die('Could not connect ' . mysql_error());
-				}
-				$selected_db = mysql_select_db("webdb13KIC1",$con);
-				$selection = mysql_query("SELECT * FROM subjects WHERE open=1 ORDER BY subject_id DESC LIMIT 0,10");
-				while($row = mysql_fetch_array($selection))
-  				{
-  					echo "<tr>";
-  					echo "<td>" . $row['subject_name'] . "</td>";
-  					echo "</tr>";
-  				}
-		
-				mysql_close($con);
-  			?>
+        <?php
+			$con = mysql_connect("localhost:3306","webdb13KIC1","busteqec"); 
+			if(!$con)
+			{
+				die('Could not connect ' . mysql_error());
+			}
+			$selected_db = mysql_select_db("webdb13KIC1",$con);
+			$selection = mysql_query("SELECT * FROM subjects WHERE open=1 ORDER BY subject_id DESC LIMIT 0,10");
+			while($row = mysql_fetch_array($selection))
+  			{
+  				echo "<tr>";
+  				echo "<td>" . $row['subject_name'] . "</td>";
+  				echo "</tr>";
+  			}
+			mysql_close($con);
+  		?>
 	</table>
-  	<table class="forumcontent" align="center">
+  	<table class="tablemember">
 	  	<tr>
    			<td class="tablehead"><strong>10 most popular subjects</strong></td>
   		</tr>
-	</table>
-    <table>
         <?php
 			$con = mysql_connect("localhost:3306","webdb13KIC1","busteqec");
 			if(!$con)
@@ -40,7 +35,7 @@
 			while($row = mysql_fetch_array($selection))
 			{
 				echo "<tr>";
-				echo "<td>" . $selection['subject_name']. "</td>" ;
+				echo "<td>" . $row['subject_name']. "</td>" ;
 				echo "</tr>";
 			}
 			mysql_close($con);
