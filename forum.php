8f690db2d76e7144bf0040b2fda1297625b1841d
jroeting <jennifer.roeting@gmail.com>
2013-01-21 12:09:16 +0100

    mysql verbinding aangepast

--- forum.php
+++ forum.php
@@ -4,10 +4,17 @@
     	<td class="tablehead"><strong>Number of subjects</td>	
     </tr>
   	<tr class="tablebody">
-  		<td><p class="blacktext"><a href="index.php?content=technical_issues" <?php if($content == technical_issues) echo "class=\"current\"" ?>><span>Technical Issues</span></a></p></td>
+  		<td><p class="blacktext"><a href="index.php?content=technical_issues"><span>Technical Issues</span></a></p></td>
     	<td>
 			<?php
-  	  			$con = mysql_connect("localhost:3306","webdb13KIC1","busteqec"); 
+                $db = new PDO('mysql:host=localhost;dbname=webdb13KIC1', 'webdb13KIC1', 'busteqec');
+                $sql = "SELECT * FROM subjects WHERE category='technical_issues'";
+                $count = mysql_num_rows($sql);
+                $results = $db->query($count);
+                echo "$results";
+                $db = NULL;
+
+  	  			/*$con = mysql_connect("localhost:3306","webdb13KIC1","busteqec"); 
 	   			if(!$con)
 		  		{	
 		    		die('Could not connect ' . mysql_error());
@@ -16,7 +23,7 @@
   	  			$selection = mysql_query("SELECT * FROM subjects WHERE category='technical_issues'");
 	  			$count = mysql_num_rows($selection);
       			echo "$count";
-	  			mysql_close($con);
+	  			mysql_close($con);*/
   			?>
         </td>
 	</tr>
@@ -24,33 +31,25 @@
   		<td><p class="blacktext"><a href="index.php?content=cartalk" <?php if($content == cartalk) echo "class=\"current\"" ?>><span>Cartalk</span></a></p></td>
     	<td>
 			<?php
-      			$con = mysql_connect("localhost:3306","webdb13KIC1","busteqec");
-      			if(!$con)
-      			{
-        		die('Could not connect ' . mysql_error());
-      			}
-      			$selection_db = mysql_select_db("webdb13KIC1",$con);
-      			$selection = mysql_query("SELECT * FROM subjects WHERE category = 'cartalk'");
-	  			$count = mysql_num_rows($selection);
-      			echo "$count";
-	  			mysql_close($con);
-    		?>
+                $db = new PDO('mysql:host=localhost;dbname=webdb13KIC1', 'webdb13KIC1', 'busteqec');
+                $sql = "SELECT * FROM subjects WHERE category='cartalk'";
+                $count = mysql_num_rows($sql);
+                $results = $db->query($count);
+                echo "$results";
+                $db = NULL;
+            ?>
 		</td>
 	</tr>
   	<tr class="tablebody" >
   		<td><p class="blacktext"><a href="index.php?content=car_unrelated" <?php if($content == car_unrelated) echo "class=\"current\"" ?>><span>Car-unrelated</span></a></p></td>
     	<td>
 			<?php
-      			$con = mysql_connect("localhost:3306","webdb13KIC1","busteqec");
-      			if(!$con)
-      			{
-        			die('Could not connect ' . mysql_error());
-      			}
-      			$selection_db = mysql_select_db("webdb13KIC1",$con);
-      			$selection = mysql_query("SELECT * FROM subjects WHERE category = 'car_unrelated'");
-	  			$count = mysql_num_rows($selection);
-      			echo "$count";
-	  			mysql_close($con);
+                $db = new PDO('mysql:host=localhost;dbname=webdb13KIC1', 'webdb13KIC1', 'busteqec');
+                $sql = "SELECT * FROM subjects WHERE category='car_unrelated'";
+                $count = mysql_num_rows($sql);
+                $results = $db->query($count);
+                echo "$results";
+                $db = NULL;
     		?>
         </td>
   	</tr>
