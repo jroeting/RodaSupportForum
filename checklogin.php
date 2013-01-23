92b09223eb4681d9a25d4c08ed225ab9fe691f8a
Max Bevelander <maxbevelander@hotmail.com>
2013-01-23 18:11:58 +0100

    foutjes

--- checklogin.php
+++ checklogin.php
@@ -9,7 +9,7 @@ $mypassword = hash('sha256', $mypassword);
 // Connect to server and select databse.
 include "db_con.php";
 
-$sql = "SELECT * FROM user_data WHERE username='$myusername' and password='$mypassword'");
+$sql = "SELECT * FROM user_data WHERE username='$myusername' and password='$mypassword'";
 $result = $db->query($sql);
 
 foreach($result as $row)
@@ -29,10 +29,6 @@ foreach($result as $row)
 		}
 	}
 }
-
-else
-{
-	echo "Invalid username/password";
-	echo "<a href=index.php?content=inlog><br />Go back</a>";
-}
+echo "Invalid username/password";
+echo "<a href=index.php?content=inlog><br />Go back</a>";
  ?>
