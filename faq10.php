771bdcf0eecdfffeb16d791b4a964e2d1729c25a
Michael Chen <mickeywhc@hotmail.com>
2013-01-23 14:37:54 +0100

    aanpassing regel

--- faq10.php
+++ faq10.php
@@ -3,10 +3,13 @@
 </h2>
 <div style='height:250px'>
 	To edit or update your profile you must first be logged into the forum. <br />
-	Once you have logged in, on the top right hand corner of the page click on the "My profile" link. <br />
+	Once you have logged in, on the top right hand corner of the page click on your username. <br />
 	Once you've clicked on the link, you will be directed to the page of your own profile. <br />
 	To edit your profile page, click on the "edit profile" link. <br />
-	Once you have completed editing your profile click on the "Update" button and all your information that you've entered will be saved. <br />
+	Once you have completed editing your profile click on the "Change Profile" button and all your information that you've entered will be saved. <br />
+	<br />
+	<i>note: You must fill in every entry, else your profile will not change. <br />
+	If you want to leave it empty, then type a space. </i>
 	<br />
 	<br />
 	<a href="index.php?content=faq"> Go Back </a>
