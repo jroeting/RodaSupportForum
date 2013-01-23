c148a39d23cd2e8a45b373e964c1ced5fd33e8e0
Max Bevelander <maxbevelander@hotmail.com>
2013-01-23 17:25:21 +0100

    checklogin met nieuwe connectie db

--- checklogin.php
+++ checklogin.php
@@ -7,39 +7,30 @@ $mypassword = $_POST['password'];
 $mypassword = hash('sha256', $mypassword);
 
 // Connect to server and select databse.
-$connectie = mysql_connect("localhost:3306", "webdb13KIC1", "busteqec");
-if(!$connectie)
+include "db_con.php";
+
+$sql = "SELECT * FROM user_data WHERE username='$myusername' and password='$mypassword'");
+$result = $db->query($sql);
+
+foreach($result as $row)
 {
-	die('Could not connect to database ' . mysql_error());
-}
-$select_db = mysql_select_db("webdb13KIC1", $connectie);
-if(!$select_db)
-{
-	die('Could not connect ' . mysql_error());
-}
-$select = mysql_query("SELECT * FROM user_data WHERE username='$myusername' and password='$mypassword'");
-if(!$select)
-{
-	echo 'Could not run query: ' . mysql_error();
-	exit;
+	if($mypassword == $row['password'] && $myusername == $row['username'])
+	{
+		$_SESSION['username'] = $row['username'];
+		$account_type = $row['account_type'];
+		if($account_type == "adm")
+		{
+			$_SESSION['account_type'] = 1;
+		}
+			
+		if(isset($_SESSION['username']))
+		{
+			header("location:index.php?content=home");
+		}
+	}
 }
 
-$row = mysql_fetch_array($select);
-
-if($mypassword == $row['password'] && $myusername == $row['username'])
-{
-	$_SESSION['username'] = $row['username'];
-	$account_type = $row['account_type'];
-	if($account_type == "adm")
-	{
-		$_SESSION['account_type'] = 1;
-	}
-		
-	if(isset($_SESSION['username']))
-	{
-		header("location:index.php?content=home");
-	}
-}else
+else
 {
 	echo "Invalid username/password";
 	echo "<a href=index.php?content=inlog><br />Go back</a>";
