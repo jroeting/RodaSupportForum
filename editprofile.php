76f9e1461492731a33750be15c48bfb40d909c2b
Max Bevelander <maxbevelander@hotmail.com>
2013-01-27 22:33:46 +0100

    kleine foutjes

--- editprofile.php
+++ editprofile.php
@@ -22,29 +22,29 @@
 				</tr> 
 				<tr>
 					<td>Personal Text: </td> 
-					<td><input type="text" name="personal_text" maxlength="75" size="75" value=" <?php echo $data_array['personal_text']; ?>"></td>
+					<td><input type="text" name="personal_text" maxlength="75" size="75" value="<?php echo $data_array['personal_text'];?>"></td>
 				</tr> 
 				<tr>
-					<td> Age: </td> 
-					<td><input type="text" name="age" value=" <?php echo $data_array['age']; ?>"></td>	
+					<td>Age: </td> 
+					<td><input type="text" name="age" value="<?php echo $data_array['age']; ?>"></td>	
 				</tr>
 				<tr>
 					<td>Gender: </td> 
-					<td><input type="radio" name="gender" id="radio" value= 0 <?php if($_POST['gender']== 0) echo 'checked="checked"'; ?> > Male </td>
+					<td><input type="radio" name="gender" id="radio" value= 0 <?php if($_POST['gender']== 0) echo 'checked="checked"';?> > Male </td>
 				</tr>
 				<tr>
-					<td> &nbsp; </td>
-					<td><input type="radio" name="gender" id="radio" value= 1 <?php if($_POST['gender']== 1) echo 'checked="checked"'; ?> > Female </td>	
+					<td>&nbsp; </td>
+					<td><input type="radio" name="gender" id="radio" value= 1 <?php if($_POST['gender']== 1) echo 'checked="checked"';?> > Female </td>	
 					
 				</tr> 
 				<tr>
-					<td> Country </td> 
-					<td><input type="text" name="country" value=" <?php echo $data_array['country']; ?>">
+					<td>Country: </td> 
+					<td><input type="text" name="country" value="<?php echo $data_array['country'];?>">
 					</td>	
 				</tr> 
 				<tr>
 					<td>Quote: </td> 
-					<td><input type="text" name="quote" maxlength="75" size="75" value="  <?php echo $data_array['quote']; ?>"></td>
+					<td><input type="text" name="quote" maxlength="75" size="75" value="<?php echo $data_array['quote'];?>"></td>
 				</tr> 
 				<tr>
 				</tr> 
