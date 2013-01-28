ec254a01ff6fc0be3029b1ad2803dc1aba09ef95
Kyllian Broers <kyllianbroers@live.nl>
2013-01-28 19:58:33 +0100

    updates

--- phpadmin.php
+++ phpadmin.php
@@ -1,26 +1,44 @@
 <!-- adminpanel.php is a page that is available for all other administrators for checking subjects and other admin related tasks -->
 <p> Welcome ad the administrator panel. Together with the other members of the administrator panel, you are responsible for approve new
 submitted subjects, take care of spam reports, block inappropriate users and delete unactivated accounts.</p>
-<form action="index.php?content=phpadmin" method="post">
-	<?php
-		function nrUnverified()
+
+<?php
+	function nrUnverified()
+	{
+		include 'db_con.php';
+		$count_unverified = $db->prepare("SELECT COUNT(*) AS nrUnverified FROM user_data WHERE verified = false AND register_date<DATE_SUB(curdate(), INTERVAL 5 DAY)");
+		if ($count_unverified->execute())
+		{
+			$row = $count_unverified->fetch();
+			return $row["nrUnverified"];
+		}
+
+		// close database
+        $db = NULL;
+	}
+	
+	if(isset($_POST["submit"]))
+	{
+		if($_POST["button"] == "remove unverified")
 		{
 			include 'db_con.php';
-			$count_unverified = $db->prepare("SELECT COUNT(*) AS nrUnverified FROM user_data WHERE verified = FALSE");
-				if ($count_unverified->execute())
-				{
-					$row = $count_unverified->fetch();
-					echo $row["nrUnverified"];
-				}
+			$remove_unverified = $db->prepare("DELETE FROM user_data WHERE verified = false AND register_date<DATE_SUB(curdate(), INTERVAL 5 DAY)");
+			$remove_unverified->execute;
+			// close database
+			$db = NULL;
 		}
-	?>
-	<input type="submit" class="button2" value="Remove <?php nrUnverified() ?> unverified users" />
+	}
+?>
+<form action="index.php?content=phpadmin" method="post">
+	<input type="hidden" name="button" value="remove unverified" />
+	<input type="submit" class="button2" name="submit" value="Remove <?php echo nrUnverified() ?> unverified users" />
 </form>
 <table width = "400">
 	<tr>
     	<td class="tablehead" colspan="2">Your colleague admins</td>
     <tr>
 	<?php
+		include 'db_con.php';
 		$myUsername = $_SESSION['username'];
 		$username;
 		$select_admins = "SELECT username, email
@@ -54,6 +72,7 @@ submitted subjects, take care of spam reports, block inappropriate users and del
 			echo "<td><a href=\"index.php?content=removesubject&action=0&subject=" . $row['subject_id'] . "\"> remove </a> | <a href=\"index.php?content=approvesubject&action=0&subject=" . $row['subject_id'] . "\"> approve </a>";
 			echo '</tr>';
 		}
+		
 	?>	
 </table>
 
@@ -71,6 +90,8 @@ submitted subjects, take care of spam reports, block inappropriate users and del
 				echo '<tr>';
 				echo "<td><a href=\"index.php?content=topic&subject=" . $row['subject_id'] . "&subjectname=" . $row['subject_name'] ."\">" .$row['subject_name'] ."</a></td></tr>";
 		}
+		
+		// close database
+        $db = NULL;
 	?>
 </table>
-							 
