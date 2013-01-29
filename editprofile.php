cc8f1e71f064e9d24bffb29b3c2506e7912bbfa6
Michael Chen <mickeywhc@hotmail.com>
2013-01-29 17:37:55 +0100

    stijl in css

--- editprofile.php
+++ editprofile.php
@@ -13,43 +13,43 @@
 			
 			// form for edit profile
 			echo "<form action=\"index.php?content=profile&user_id=" . $data_array["user_id"] . "\" method=\"post\">"; ?>
-			<table cellpadding="10">
+			<table class="editprofile">
 				<tr>
 					<td>Avatar &#60;200kB</td>
 					<td><input type="hidden" name="MAX_FILE_SIZE" value="200000" />
-						<input type="file" name="file" id="file" accept="image/*" />
+						<input type="file" name="avatar" id="file" accept="image/*" />
 					</td>
 				</tr> 
 				<tr>
 					<td>Personal Text: </td> 
-					<td><input type="text" name="personal_text" maxlength="75" size="75" value="<?php echo $data_array['personal_text'];?>"></td>
+					<td><input type="text" name="personal_text" maxlength="75" size="75" value=" <?php echo $data_array['personal_text']; ?>"/></td>
 				</tr> 
 				<tr>
-					<td>Age: </td> 
-					<td><input type="text" name="age" value="<?php echo $data_array['age']; ?>"></td>	
+					<td> Age: </td> 
+					<td><input type="text" name="age" value=" <?php echo $data_array['age']; ?>"/></td>	
 				</tr>
 				<tr>
 					<td>Gender: </td> 
-					<td><input type="radio" name="gender" id="radio" value= 0 <?php if($_POST['gender']== 0) echo 'checked="checked"';?> > Male </td>
+					<td><input type="radio" name="gender" id="radio" value= "0" <?php if($_POST['gender']== 0) echo 'checked="checked"'; ?> /> Male </td>
 				</tr>
 				<tr>
-					<td>&nbsp; </td>
-					<td><input type="radio" name="gender" id="radio" value= 1 <?php if($_POST['gender']== 1) echo 'checked="checked"';?> > Female </td>	
+					<td> &nbsp; </td>
+					<td><input type="radio" name="gender" id="radio" value= "1" <?php if($_POST['gender']== 1) echo 'checked="checked"'; ?> /> Female </td>	
 					
 				</tr> 
 				<tr>
-					<td>Country: </td> 
-					<td><input type="text" name="country" value="<?php echo $data_array['country'];?>">
+					<td> Country </td> 
+					<td><input type="text" name="country" value=" <?php echo $data_array['country']; ?>" />
 					</td>	
 				</tr> 
 				<tr>
 					<td>Quote: </td> 
-					<td><input type="text" name="quote" maxlength="75" size="75" value="<?php echo $data_array['quote'];?>"></td>
+					<td><input type="text" name="quote" maxlength="75" size="75" value="  <?php echo $data_array['quote']; ?>" /></td>
 				</tr> 
 				<tr>
 				</tr> 
 				<tr>
-					<td><input type="submit" name="submit" value="Change Profile"></td>
+					<td><input type="submit" name="submit" value="Change Profile" /></td>
 				</tr>
 			</table>
 		</form>
--- members.php
+++ members.php
@@ -6,13 +6,13 @@
 		Members of Roda
 	</h1>
 	 
-	<table class="tablemember" border="1" cellpadding="10">
+	<table class="memberlist">
   		<tr>
-			<th class="tablehead"><strong> Username </strong></th>
-			<th class="tablehead"><strong> Email </strong> </th>
-			<th class="tablehead"><strong> Posts </strong> </th>
-			<th class="tablehead"><strong> Account Type </strong></th>
-			<th class="tablehead"><strong> Date Registered </strong></th>
+			<th class="tablehead2"><strong> Username </strong></th>
+			<th class="tablehead2"><strong> Email </strong> </th>
+			<th class="tablehead2"><strong> Posts </strong> </th>
+			<th class="tablehead2"><strong> Account Type </strong></th>
+			<th class="tablehead2"><strong> Date Registered </strong></th>
         </tr>
 		<?php
 			include "db_con.php";
@@ -23,16 +23,16 @@
 			// shows the memberlist with info of members
 			foreach($result as $row) 
 			{
-				echo "<tr>";
-				echo "<td>" . "<a href=\"index.php?content=profile&user_id=" . $row["user_id"] . "\">" . $row["username"] . "</a>" . "</td>";
-				echo "<td>" . $row['email'] . "</td>";
+				echo "<tr class=cellpadding>";
+				echo "<td class=cellpadding>" . "<a href=\"index.php?content=profile&user_id=" . $row["user_id"] . "\">" . $row["username"] . "</a>" . "</td>";
+				echo "<td class=cellpadding>" . $row['email'] . "</td>";
 				
 				$userID = $row['user_id'];
 				// Query to count number of posts for each member
 				$numberOfPosts = "SELECT count(*) FROM posts WHERE user_id = $userID";
 				$count = $db->query($numberOfPosts);
 				$data_array = $count->fetch();
-				echo "<td>" . $data_array[0] . "</td>";
+				echo "<td class=cellpadding>" . $data_array[0] . "</td>";
 				switch ($row["account_type"]) {
 								case "adm": 
 									$accountType = "admin";
@@ -41,8 +41,8 @@
 									$accountType = "user";
 									break;
 							}
-				echo "<td>" . $accountType . "</td>";	
-				echo "<td>" . $row['register_date'] . "</td>";
+				echo "<td class=cellpadding>" . $accountType . "</td>";	
+				echo "<td class=cellpadding>" . $row['register_date'] . "</td>";
 				echo "</tr>";
 			}
 	
