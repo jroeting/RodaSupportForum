f8e57b1ce3501de068aed336a9777f93007a42c3
Jennifer Roeting <jennifer.roeting@gmail.com>
2013-01-29 08:54:21 +0100

    bindvalue in newpost

--- newpost.php
+++ newpost.php
@@ -5,8 +5,10 @@
 		// connect with database
         include 'db_con.php';
 		// selection of all subjects, ordered by subject_id (so most recent is on top)
-        $sql= "SELECT subject_name FROM subjects WHERE subject_name='$_POST[title]'";
-		$results = $db->query($sql);
+        $sql= "SELECT subject_name FROM subjects WHERE subject_name=?";
+		$results = $db->prepare($sql);
+		$results->bindValue(1,$_POST['title'],PDO::PARAM_STR);
+		$results->execute();
 		foreach($results as $row)
         {
             if ($row['subject_name'] == $_POST['title'])
@@ -43,24 +45,37 @@
 			// connect with database
             include 'db_con.php';
 			// selection of all subjects, ordered by subject_id (so most recent is on top)
-            $sqlSelectUserId = "SELECT user_id FROM user_data WHERE username='$GLOBALS[username]'";
-			$results = $db->query($sqlSelectUserId);
+            $sqlSelectUserId = "SELECT user_id FROM user_data WHERE username=?";
+			$results = $db->prepare($sqlSelectUserId);
+			$results->bindValue(1,$GLOBALS['username'],PDO::PARAM_STR);
+			$results->execute();
 			foreach($results as $row)
             {
                 $userId = $row['user_id'];
             }
-			$sqlinsert = "INSERT INTO subjects (user_id, subject_name, category) VALUES ('$userId','$title','$category')";
-            $input = $db->query($sqlinsert);
+			$sqlinsert = "INSERT INTO subjects (user_id, subject_name, category) VALUES (?,?,?)";
+            $input = $db->prepare($sqlinsert);
+			$input->bindValue(1,$userId,PDO::PARAM_INT);
+			$input->bindValue(2,$title,PDO::PARAM_STR);
+			$input->bindValue(3,$category,PDO::PARAM_STR);
+			$input->execute();
 			
-			$sqlSelectSubjectId = "SELECT subject_id FROM subjects WHERE (user_id='$userId' AND subject_name='$title')";
-			$results = $db->query($sqlSelectSubjectId);
+			$sqlSelectSubjectId = "SELECT subject_id FROM subjects WHERE (user_id = :userId AND subject_name = :title)";
+			$results = $db->prepare($sqlSelectSubjectId);
+			$results->bindValue(':userId',$userId,PDO::PARAM_INT);
+			$results->bindValue(':title',$title,PDO::PARAM_STR);
+			$results->execute();
 			foreach($results as $row)
             {
                 $subjectId = $row['subject_id'];
             }
 			
-			$sqlinsert = "INSERT INTO posts (subject_id, user_id, content) VALUES ('$subjectId','$userId','$post')";
-			$input = $db->query($sqlinsert);
+			$sqlinsert = "INSERT INTO posts (subject_id, user_id, content) VALUES (?,?,?)";
+			$input = $db->prepare($sqlinsert);
+			$input->bindValue(1,$subjectId,PDO::PARAM_INT);
+			$input->bindValue(2,$userId,PDO::PARAM_INT);
+			$input->bindValue(3,$post,PDO::PARAM_STR);
+			$input->execute();
 			
 			// close database
             $db = NULL;
