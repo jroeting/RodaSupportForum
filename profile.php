e1c39b44a3039945c13b821ba417dd7fe803e3a3
Michael Chen <mickeywhc@hotmail.com>
2013-01-30 19:10:37 +0100

    na submission updates direct te zien

--- profile.php
+++ profile.php
@@ -1,4 +1,151 @@
-	<!-- Layout Profile -->
+	<!-- Update Profile -->
+	<?php
+		// variables that contains form errors of the user
+		$errorFile = ""; // avatar error
+		// adds slashes and read the contents of a file into a string
+		$imgData = addslashes (file_get_contents("images/avatar.png")); 
+
+		/**** Functions ****/ 
+
+		//checks the file for several conditions, errormessages will be assigned if conditions dont pass
+		function checkFile()
+		{
+			$allowedExts = array("jpg", "jpeg", "gif", "png");
+			$extension = end(explode(".", $_FILES["file"]["name"]));
+
+			if ($_FILES["file"]["name"] != "")
+			{
+				$allowedExts = array("jpg", "jpeg", "gif", "png");
+				$extension = end(explode(".", $_FILES["file"]["name"]));
+
+				//checks for right extension, size ,height and width
+				if ((($_FILES["file"]["type"] == "image/gif")
+				|| ($_FILES["file"]["type"] == "image/jpeg")
+				|| ($_FILES["file"]["type"] == "image/png")
+				|| ($_FILES["file"]["type"] == "image/pjpeg"))
+				&& ($_FILES["file"]["size"] < 200000)
+				&& in_array($extension, $allowedExts))
+				{
+					$size = getimagesize($_FILES['file']['tmp_name']);
+
+					if ($_FILES["file"]["error"] > 0)
+					{
+						$GLOBALS['errorFile'] = "file error";
+					}elseif ($size["0"] > 1000 && $size["1"] > 1000)
+					{
+						$GLOBALS['errorFile'] = "file width or height must be less then 1000px";
+					}else
+					{
+						$GLOBALS['imgData'] = fopen($_FILES['file']['tmp_name'], 'rb');
+					}
+				}
+				else
+				{
+					$GLOBALS['errorFile'] = "invalid file, only .gif, .jpg, .jpg or .png and less then 200kb ";
+				}
+
+
+			}
+		}
+
+		// checks personal text in profile
+		function checkPersonalText()
+		{	
+			$_POST['personal_text'] = strip_tags($_POST['personal_text']); // strip HTML and PHP tags from a string
+			$_POST['personal_text'] = htmlentities($_POST['personal_text'], ENT_QUOTES); // will convert both double and single quotes to html entities
+			$_POST['personal_text'] = trim($_POST['personal_text']); // strip whitespace from the beginning and end of a string
+			$_POST['personal_text'] = filter_var($_POST['personal_text'], FILTER_SANITIZE_STRING);// Filters a variable with FILTER_SANITIZE_STRING		
+		}
+
+		// check the input form for quote
+		function checkQuote()
+		{
+			$_POST['quote'] = strip_tags($_POST['quote']); // strip HTML and PHP tags from a string
+			$_POST['quote'] = htmlentities($_POST['quote'], ENT_QUOTES); // will convert both double and single quotes to html entities
+			$_POST['quote'] = trim($_POST['quote']); // strip whitespace from the beginning and end of a string
+			$_POST['quote'] = filter_var($_POST['quote'], FILTER_SANITIZE_STRING);// Filters a variable with FILTER_SANITIZE_STRING	
+		}
+
+		// Updates the database, thus updates the profile page
+		function inputForm()
+		{
+			if($GLOBALS['errorFile'] == "")
+			{
+				include 'db_con.php';
+
+				$userID = $_GET['user_id']; 
+
+				// checks whether a country exists
+				$country = $_POST['country']; // input of user in country
+				$sql= "SELECT country FROM country_list"; 
+				$countryCheck = $db->query($sql);
+
+			// checks whether a country exists
+				foreach($countryCheck as $row)
+				{
+					$contains = false;
+
+					if ($country == $row['country']) 
+					{	
+						$contains = true;
+					} 
+				}
+				if($contains = true)
+				{
+					$selection = "UPDATE user_data SET avatar=?, personal_text=?, age=?, gender=?, country=?, quote=? WHERE user_id= ? LIMIT 1";
+					$result = $db->prepare($selection);
+					$result->bindValue(1, $_POST['file'], PDO::PARAM_LOB);
+					$result->bindValue(2, $_POST['personal_text'], PDO::PARAM_STR);
+					$result->bindValue(3, $_POST['age'], PDO::PARAM_INT);
+					$result->bindValue(4, $_POST['gender'], PDO::PARAM_BOOL);
+					$result->bindValue(5, $_POST['country'], PDO::PARAM_STR);
+					$result->bindValue(6, $_POST['quote'], PDO::PARAM_STR);
+					$result->bindValue(7, $userID, PDO::PARAM_INT);
+					$result->execute();
+				}
+				else
+				{
+					$selection2 = "UPDATE user_data SET avatar=?, personal_text=?, age=?, gender=?, quote=? WHERE user_id= ? LIMIT 1";
+					$result = $db->prepare($selection2);
+					$result->bindValue(1, $_POST['file'], PDO::PARAM_LOB);
+					$result->bindValue(2, $_POST['personal_text'], PDO::PARAM_STR);
+					$result->bindValue(3, $_POST['age'], PDO::PARAM_INT);
+					$result->bindValue(4, $_POST['gender'], PDO::PARAM_BOOL);
+					$result->bindValue(5, $_POST['quote'], PDO::PARAM_STR);
+					$result->bindValue(6, $userID, PDO::PARAM_INT);
+					$result->execute(); 
+				}
+			}		
+		}
+			// functions only run after form submission
+			if(isset($_POST["submit"]))
+			{	
+				if(isset($_POST['file'])){ $avatar = $_POST['file']; }
+				$personalText = $_POST['personal_text'];
+				$age = $_POST['age'];
+				$gender = $_POST['gender'];
+				$country = $_POST['country'];
+				$quote = $_POST['quote'];
+
+				if(!isset($_POST['file']))
+				{
+					checkPersonalText();
+					checkQuote();
+					inputForm();
+				}
+				else
+				{				
+					checkFile();
+					checkPersonalText();
+					checkQuote();
+					inputForm();
+				}
+			}
+
+			$db=NULL; // closes database
+	?>
+
+<!-- Output Layout Profile -->
 <?php
 	// the user can only enter the profile page if he/she is logged in
 	if(isset($_SESSION['username'])) :
@@ -21,13 +168,13 @@
 					echo "<br /><br />";
 					echo "Username: " . $row["username"];  
 					echo "<br /><br />";
-					
+
 					switch ($row["account_type"]) 
 					{
                         case "adm": 
                         $accountType = "admin";
                         break;
-						
+
                         case "usr": 
                         $accountType = "user";
                         break;
@@ -103,7 +250,7 @@ EOT;
 					echo "<td> Female </td>";
 				}
 				echo "</tr>";
-				
+
 				echo <<<EOT
 					<tr>
 						<td> &nbsp; </td>
@@ -138,158 +285,13 @@ EOT;
 				if($_SESSION['user_id'] == $row['user_id'] && $_SESSION['account_type'] == 1) 
 				{
 					echo '<br/><strong><a href="index.php?content=phpadmin">Go to administrator panel</a></strong>';
-					
+
 				}
 			?>
 		</div>	
 	</div> 
 	
-	<!-- Update Profile -->
-	<?php
-		// variables that contains form errors of the user
-		$errorFile = ""; // avatar error
-		// adds slashes and read the contents of a file into a string
-		$imgData = addslashes (file_get_contents("images/avatar.png")); 
-		
-		/**** Functions ****/ 
-		
-		//checks the file for several conditions, errormessages will be assigned if conditions dont pass
-		function checkFile()
-		{
-			$allowedExts = array("jpg", "jpeg", "gif", "png");
-			$extension = end(explode(".", $_FILES["file"]["name"]));
-			
-			if ($_FILES["file"]["name"] != "")
-			{
-				$allowedExts = array("jpg", "jpeg", "gif", "png");
-				$extension = end(explode(".", $_FILES["file"]["name"]));
-				
-				//checks for right extension, size ,height and width
-				if ((($_FILES["file"]["type"] == "image/gif")
-				|| ($_FILES["file"]["type"] == "image/jpeg")
-				|| ($_FILES["file"]["type"] == "image/png")
-				|| ($_FILES["file"]["type"] == "image/pjpeg"))
-				&& ($_FILES["file"]["size"] < 200000)
-				&& in_array($extension, $allowedExts))
-				{
-					$size = getimagesize($_FILES['file']['tmp_name']);
-					
-					if ($_FILES["file"]["error"] > 0)
-					{
-						$GLOBALS['errorFile'] = "file error";
-					}elseif ($size["0"] > 1000 && $size["1"] > 1000)
-					{
-						$GLOBALS['errorFile'] = "file width or height must be less then 1000px";
-					}else
-					{
-						$GLOBALS['imgData'] = fopen($_FILES['file']['tmp_name'], 'rb');
-					}
-				}
-				else
-				{
-					$GLOBALS['errorFile'] = "invalid file, only .gif, .jpg, .jpg or .png and less then 200kb ";
-				}
-				
-				
-			}
-		}
-		
-		// checks personal text in profile
-		function checkPersonalText()
-		{	
-			$_POST['personal_text'] = strip_tags($_POST['personal_text']); // strip HTML and PHP tags from a string
-			$_POST['personal_text'] = htmlentities($_POST['personal_text'], ENT_QUOTES); // will convert both double and single quotes to html entities
-			$_POST['personal_text'] = trim($_POST['personal_text']); // strip whitespace from the beginning and end of a string
-			$_POST['personal_text'] = filter_var($_POST['personal_text'], FILTER_SANITIZE_STRING);// Filters a variable with FILTER_SANITIZE_STRING		
-		}
-			
-		// check the input form for quote
-		function checkQuote()
-		{
-			$_POST['quote'] = strip_tags($_POST['quote']); // strip HTML and PHP tags from a string
-			$_POST['quote'] = htmlentities($_POST['quote'], ENT_QUOTES); // will convert both double and single quotes to html entities
-			$_POST['quote'] = trim($_POST['quote']); // strip whitespace from the beginning and end of a string
-			$_POST['quote'] = filter_var($_POST['quote'], FILTER_SANITIZE_STRING);// Filters a variable with FILTER_SANITIZE_STRING	
-		}
-		
-		// Updates the database, thus updates the profile page
-		function inputForm()
-		{
-			if($GLOBALS['errorFile'] == "")
-			{
-				include 'db_con.php';
-				
-				$userID = $_GET['user_id']; 
-									
-				// checks whether a country exists
-				$country = $_POST['country']; // input of user in country
-				$sql= "SELECT country FROM country_list"; 
-				$countryCheck = $db->query($sql);
-								
-			// checks whether a country exists
-				foreach($countryCheck as $row)
-				{
-					$contains = false;
-					
-					if ($country == $row['country']) 
-					{	
-						$contains = true;
-					} 
-				}
-				if($contains = true)
-				{
-					$selection = "UPDATE user_data SET avatar=?, personal_text=?, age=?, gender=?, country=?, quote=? WHERE user_id= ? LIMIT 1";
-					$result = $db->prepare($selection);
-					$result->bindValue(1, $_POST['file'], PDO::PARAM_LOB);
-					$result->bindValue(2, $_POST['personal_text'], PDO::PARAM_STR);
-					$result->bindValue(3, $_POST['age'], PDO::PARAM_INT);
-					$result->bindValue(4, $_POST['gender'], PDO::PARAM_BOOL);
-					$result->bindValue(5, $_POST['country'], PDO::PARAM_STR);
-					$result->bindValue(6, $_POST['quote'], PDO::PARAM_STR);
-					$result->bindValue(7, $userID, PDO::PARAM_INT);
-					$result->execute();
-				}
-				else
-				{
-					$selection2 = "UPDATE user_data SET avatar=?, personal_text=?, age=?, gender=?, quote=? WHERE user_id= ? LIMIT 1";
-					$result = $db->prepare($selection2);
-					$result->bindValue(1, $_POST['file'], PDO::PARAM_LOB);
-					$result->bindValue(2, $_POST['personal_text'], PDO::PARAM_STR);
-					$result->bindValue(3, $_POST['age'], PDO::PARAM_INT);
-					$result->bindValue(4, $_POST['gender'], PDO::PARAM_BOOL);
-					$result->bindValue(5, $_POST['quote'], PDO::PARAM_STR);
-					$result->bindValue(6, $userID, PDO::PARAM_INT);
-					$result->execute(); 
-				}
-			}		
-		}
-			// functions only run after form submission
-			if(isset($_POST["submit"]))
-			{	
-				if(isset($_POST['file'])){ $avatar = $_POST['file']; }
-				$personalText = $_POST['personal_text'];
-				$age = $_POST['age'];
-				$gender = $_POST['gender'];
-				$country = $_POST['country'];
-				$quote = $_POST['quote'];
-				
-				if(!isset($_POST['file']))
-				{
-					checkPersonalText();
-					checkQuote();
-					inputForm();
-				}
-				else
-				{				
-					checkFile();
-					checkPersonalText();
-					checkQuote();
-					inputForm();
-				}
-			}
-			
-			$db=NULL; // closes database
-	?>
+
 					
 	<?php
 		else :
